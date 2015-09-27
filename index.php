b124c7e8e90f5fb1a1d254cc6f1a4b8c406b8fe6
Osvaldo Nanastonis <obninformatica@gmail.com>
2015-09-26 22:23:34 -0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,77 @@
+<?php
+	
+?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv='Content-Type' content='text/html; charset=UTF-8'/>
+<title>OBNINFORMATICA - Desenvolvimentos - Services</title>
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<link rel="stylesheet" href="fonts/font.css" />
+<link rel='shortcut icon' href='images/wikipedia.ico' />
+<link type='text/css' rel='stylesheet' href='css/estilo.css' />
+<script type="text/javascript" src="jquery/191/jquery.js"></script>
+<script type="text/javascript"></script>
+</head>
+<body>
+<div id="topo">
+	<div class="logo"></div>
+	<div class="descricao"></div>
+	<div class="menus">
+		<ul>
+			<li><a href="index.php">Home</a></li>
+			<li><a href="vaquinha.php">Vaquinha</a></li>
+			<li><a href="esquerda.php">Esquerda</a></li>
+			<li><a href="#">FOUR</a></li>
+			<li><a href="#">FIVE</a></li>
+			<li><a href="#">SIX</a></li>
+			<li><a href="#">SEVEN</a></li>
+			<li><a href="#">EIGHT</a></li>
+			<li><a href="#">NINE</a></li>
+			<li><a href="#">TEEN</a></li>
+		</ul>
+	</div>
+</div><!-- Topo - final -->
+
+<div id="conteudo">
+	<div class="esquerda"></div>
+	<div class="direita">
+		<h1>Dynamic Host Configuration Protocol </h1>
+		<p>O <b>DHCP</b>, Dynamic Host Configuration Protocol (Protocolo de configuração dinâmica de host), é um protocolo de serviço TCP/IP que oferece configuração dinâmica de terminais, com concessão de endereços IP de host, Máscara de sub-rede, Default Gateway (Gateway Padrão), Número IP de um ou mais servidores DNS, Número IP de um ou mais servidores WINS e Sufixos de pesquisa do DNS. Este protocolo é o sucessor do BOOTP que, embora mais simples, tornou-se limitado para as exigências atuais. O DHCP surgiu como padrão em Outubro de 1993. O RFC 2131 contém as especificações mais atuais (março de 1997). O último standard para a especificação do DHCP sobre IPv6 (DHCPv6) foi publicado a Julho de 2003 como RFC 3315.</p>
+		<ul>
+			<li>Índice</li>
+			<li><b>1 - </b>Funcionamento Básico</li>
+			<li>2 Termos Utilizados no DHCP</li>
+			<li>3 Critérios de atribuição de IPs</li>
+			<li>4 Detalhes técnicos
+				<ul>
+					<li>4.1 DHCP descoberta</li>
+					<li>4.2 DHCP oferta</li>
+					<li>4.3 DHCP pedido</li>
+					<li>4.4 DHCP de confirmação</li>
+					<li>4.5 DHCP informações</li>
+					<li>4.6 DHCP liberando</li>
+					<li>4.7 Cliente parâmetros de configuração no DHCP</li>
+					<li>4.8 Identificação do fornecedor</li>
+					<li>4.9 Retransmissão DHCP</li>
+				</ul>
+			</li>
+			<li>5 Confiabilidade</li>
+			<li>6 Segurança</li>
+			<li>7 Rede sem DHCP - APIPA</li>
+			<li>8 Ver também</li>
+			<li>9 Referências</li>
+		</ul>
+	<h1>Funcionamento Básico</h1>
+	<p>Resumidamente, o DHCP opera da seguinte forma:
+
+    Um cliente envia um pacote UDP em broadcast (destinado a todas as máquinas) com uma requisição DHCP (para a porta 67);<br />
+    Os servidores DHCP que capturarem este pacote irão responder (se o cliente se enquadrar numa série de critérios — ver abaixo) para a porta 68 do Host solicitante com um pacote com configurações onde constará, pelo menos, um endereço IP, uma máscara de rede e outros dados opcionais, como o gateway, servidores de DNS, etc...<br />
+    O DHCP usa um modelo cliente-servidor, no qual o servidor DHCP mantém o gerenciamento centralizado dos endereços IP usados na rede.</p>
+  
+</div><!-- Class = Direita - Fechado -->
+<div class="limpar"></div>
+</div><!-- ID = Conteudo - Fechado -->
+
+</body>
+</html>
